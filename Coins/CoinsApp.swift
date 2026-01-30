c2f867cf3d8eee652972dc32d5bb8711da0cea83
Maksym <maksimlotickij@gmail.com>
2026-01-30 12:50:57 +0100

    Initial Commit

---
+++ Coins/CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsApp.swift
+//  Coins
+//
+//  Created by Максим Лотіцький on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
